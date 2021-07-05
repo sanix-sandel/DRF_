54b7e4b699a42bb12c2cff47a4c35b1a7d26bdbe
sanix <sanicksikani@gmail.com>
2021-07-05 17:02:12 +0300

    Generic class-based views

--- games/rest/serializers.py
+++ games/rest/serializers.py
@@ -1,14 +1,76 @@
 from rest_framework import serializers
-from .models import Game
+from .models import *
+from . import views
 
 
-class GameSerializer(serializers.ModelSerializer):
+class GameCategorySerializer(serializers.HyperlinkedModelSerializer):
+    games=serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='game-detail'
+    )
+
+    class Meta:
+        model=GameCategory
+        fields=(
+            'url',
+            'pk',
+            'name',
+            'games'
+        )
+
+class GameSerializer(serializers.HyperlinkedModelSerializer):
+    game_category=serializers.SlugRelatedField(queryset=GameCategory.objects.all(), slug_field='name')
     class Meta:
         model=Game
         fields=(
-            'id',
+            'url',
             'name', 
             'release_date',
             'game_category',
             'played'
+        )
+
+class ScoreSerializer(serializers.HyperlinkedModelSerializer):
+    game=GameSerializer()
+    class Meta:
+        model=PlayerScore
+        fields=(
+            'url',
+            'pk',
+            'score',
+            'score_date',
+            'game',
+        )        
+
+class PlayerSerializer(serializers.HyperlinkedModelSerializer):
+    scores=ScoreSerializer(many=True, read_only=True)
+    gender=serializers.ChoiceField(
+        choices=Player.GENDER_CHOICES
+    )        
+    gender_description=serializers.CharField(source='get_gender_display', read_only=True)
+
+    class Meta:
+        model=Player
+        fields=(
+            'url',
+            'name',
+            'gender',
+            'gender_description',
+            'scores',
+        )
+
+class PlayerScoreSerializer(serializers.ModelSerializer):
+    player=serializers.SlugRelatedField(queryset=Player.objects.all(), slug_field='name')        
+    game=serializers.SlugRelatedField(queryset=Game.objects.all(), slug_field='name')
+
+    class Meta:
+        model=PlayerScore
+        fields=(
+            'url',
+            'pk',
+            'score',
+            'score_date',
+            'player',
+            'game',
         )
--- games/rest/views.py
+++ games/rest/views.py
@@ -1,43 +1,65 @@
-from rest_framework.parsers import JSONParser
-from rest_framework import status
-from rest_framework.decorators import api_view
+from .models import*
+from .serializers import *
+from rest_framework import generics
 from rest_framework.response import Response
-from .models import Game
-from .serializers import GameSerializer
+from rest_framework.reverse import reverse
 
 
+class GameCategoryList(generics.ListCreateAPIView):
+    queryset = GameCategory.objects.all()
+    serializer_class = GameCategorySerializer
+    name = 'gamecategory-list'
 
-@api_view(['GET', 'POST'])
-def game_list(request):
-    if request.method=='GET':
-        games=Game.objects.all()
-        games_serializer=GameSerializer(games, many=True)    
-        return Response(games_serializer.data)
 
-    elif request.method=='POST':
-        game_serializer=GameSerializer(data=request.data)    
-        if game_serializer.is_valid():
-            game_serializer.save()
-            return Response(game_serializer.data, status=status.HTTP_201_CREATED)
-        return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)   
+class GameCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = GameCategory.objects.all()
+    serializer_class = GameCategorySerializer
+    name = 'gamecategory-detail'
 
-@api_view(['GET', 'PUT', 'POST'])
-def game_detail(request, pk):
-    try:
-        game=Game.objects.get(pk=pk)
-    except Game.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)  
 
-    if request.method=='GET':
-        game_serializer=GameSerializer(game)
-        return Response(game_serializer.data)
+class GameList(generics.ListCreateAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    name = 'game-list'
 
-    elif request.method == 'PUT':
-        game_serializer = GameSerializer(game, data=request.data)
-        if game_serializer.is_valid():
-            game_serializer.save()
-            return Response(game_serializer.data)
-        return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        game.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)                        
+
+class GameDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    name = 'game-detail'
+
+
+class PlayerList(generics.ListCreateAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    name = 'player-list'
+
+
+class PlayerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    name = 'player-detail'
+
+
+class PlayerScoreList(generics.ListCreateAPIView):
+    queryset = PlayerScore.objects.all()
+    serializer_class = PlayerScoreSerializer
+    name = 'playerscore-list'
+
+
+class PlayerScoreDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PlayerScore.objects.all()
+    serializer_class = PlayerScoreSerializer
+    name = 'playerscore-detail'
+
+
+class ApiRoot(generics.GenericAPIView):
+    name='api-root'
+    
+    def get(self, request, *args, **kwargs):
+        return Response(
+            {'players':reverse(PlayerList.name, request=request),
+            'game-categories':reverse(GameCategoryList.name, request=request),
+            'games':reverse(GameList.name, request=request),
+            'scores':reverse(PlayerScoreList.name, request=request)    
+        })
